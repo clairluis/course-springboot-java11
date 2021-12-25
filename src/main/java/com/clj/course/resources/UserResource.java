a07e584f055bcf96c251b8629f81cdd011052b05
Clair Luis Jevinski <cl.jevinski@hotmail.com>
2021-12-25 15:58:59 -0300

    User insert

--- src/main/java/com/clj/course/resources/UserResource.java
+++ src/main/java/com/clj/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.clj.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.clj.course.entities.User;
 import com.clj.course.services.UserService;
@@ -15,22 +19,31 @@ import com.clj.course.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
-		
+
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(user);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(user);
 	}
 
 }
--- src/main/java/com/clj/course/services/UserService.java
+++ src/main/java/com/clj/course/services/UserService.java
@@ -24,5 +24,9 @@ public class UserService {
 		
 		return user.get();
 	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
 
 }
